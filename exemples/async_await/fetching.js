b83be1a23ea40a8711dcbcb761e146176b082065
binarymath <arthguins@gmail.com>
2022-06-01 11:25:58 -0300

    feature: Insert reduce1

---
+++ exemples/async_await/fetching.js
@@ -0,0 +1,10 @@
+async function getData() {
+    try {
+      const response = await fetch('https://api.example.com/data');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  
